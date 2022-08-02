0fb7cc98ae84fbd93cacaea260efb18141c3da83
Yakup Ulu <yakup1720@gmail.com>
2022-08-02 15:57:01 +0300

    vucut endeksi hesaplama

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    double boy,kilo,endeks;
+
+    Scanner inp=new Scanner(System.in);
+        System.out.print("Boyunuzu metre cinsinden giriniz:");
+        boy=inp.nextDouble();
+        System.out.print("Kilonuzu kg cinsinden giriniz:");
+        kilo=inp.nextDouble();
+
+        endeks=kilo/(boy*boy);
+
+        System.out.println("Vucut Kitle Endeksiniz:"+endeks);
+
+    }
+}
